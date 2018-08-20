36ab542560b345fcbc589eb30fb75e25f22beda9
Matti Yeshurun <matti.yeshurun@gmail.com>
2018-08-20 18:10:29 +0300

    GEOCODING

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,11 @@
+class BusinessesController < ApplicationController
+  def index
+    @businesses = Business.where.not(latitude: nil, longitude: nil)
+
+    @markers = @businesses.map do |business|
+      {
+        lat: business.latitude,
+        lng: business.longitude,
+      }
+  end
+end
