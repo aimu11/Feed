9cacb07ac2aadd5b3a53802517749eb271d94a89
Amir M <amcoder2@mail.com>
2018-08-20 14:53:42 +0300

    foods controller, customers controller wip

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,28 @@
 class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+  private
+
+  def find_customer
+    @customers = Customer.find(params[food_params])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :phone_number)
+  end
 end
--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,44 @@
 class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    find_foods
+    @foods
+  end
+
+  def new
+    @foods = Food.new(food_params)
+  end
+
+  def create
+    @foods = Food.new(food_params)
+    @food.save
+  end
+
+  def edit
+    find_foods
+  end
+
+  def update
+    find_foods
+    @foods.update(food_params)
+  end
+
+  def destroy
+    find_foods
+  end
+
+  private
+
+  def find_foods
+    @foods = Food.find(params[food_params])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :description, :price, :portion, :order_before, :pickup_start, :pickup, :end, :dietary_category, :business_id)
+  end
+
+
 end
