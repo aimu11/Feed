2335fae239a90afe49d20deeb21a823d827261cb
Amir M <amcoder2@mail.com>
2018-08-22 17:42:19 +0300

    basic functions of current orders page and profile info on user dashboard

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,21 +7,31 @@ class CustomersController < ApplicationController
   end
 
   def dashboard
+    @customer = current_user.customer
   end
 
   def new
   end
 
   def edit
+    @customer = current_user.customer
   end
 
   def update
+    @customer = current_user.customer
+
+    if @customer.update(customer_params)
+    redirect_to dashboard_path
+
+  else
+    render :edit
+  end
   end
 
   private
 
   def find_customer
-    @customers = Customer.find(params[food_params])
+    @customers = Customer.find([:id])
   end
 
   def customer_params
