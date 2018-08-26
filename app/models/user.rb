1bfdec301e0bfb10ae5fad6aeb12c7f800e9b532
Amir M <amcoder2@mail.com>
2018-08-26 15:28:15 +0300

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,3 +8,21 @@ class User < ApplicationRecord
   has_one :customer, dependent: :destroy
 
 end
+
+  # new_customer = Customer.new
+  # new_customer.user = self
+  # self.save
+  # new_customer.save
+  #
+  # after_save :assign_customer_id_to_new_user
+  #
+  # def assign_customer_id_to_new_user
+  #   self.customer = Customer.new
+  #   self.save
+  #
+  #   if self.save
+  #     redirect_to dashboard_path
+  #   else
+  #     redirect_to foods_path
+  #   end
+  # end
