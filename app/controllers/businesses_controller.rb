a38ff2259cf9157b8471b5b82513d1c0679f03ed
Matti Yeshurun <matti.yeshurun@gmail.com>
2018-08-26 12:45:41 +0300

    homepage footer and foods index geocode fixed

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,13 +1,6 @@
 class BusinessesController < ApplicationController
   def index
     @businesses = Business.where.not(latitude: nil, longitude: nil)
-
-    @markers = @businesses.map do |business|
-      {
-        lat: business.latitude,
-        lng: business.longitude,
-      }
-    end
   end
 
   def dashboard
--- app/models/business.rb
+++ app/models/business.rb
@@ -2,9 +2,4 @@ class Business < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :orders, through: :foods
 
-
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
-
-  belongs_to :user
 end
