96a8341ceb5ac68742f1c87e7d6301dc9c379542
Jeremie Sayag <jeremiesayag.priv@gmail.com>
2018-08-26 19:20:43 +0300

    v

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,3 +4,24 @@ class Order < ApplicationRecord
 
   monetize :price_cents
 end
+
+
+# make a new table "cart_items"
+# user_id: cart_owner
+# food_id: food
+
+# class User
+# has_many :cart_items
+
+# class CartItemsController
+#
+#   def show
+#     @cart_items = current_user.cart_items
+#     @foods = current_user.cart_items.map(&:food)
+#
+#   def add_item
+#     @item = CardItem.new(params[:food_id])
+#     @item.user = current_user
+#     if @item.save # etc..
+#
+#
