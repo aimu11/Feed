72d36cc8a571326106c4145ae5644a50b74a2ab6
Amir M <amcoder2@mail.com>
2018-08-29 19:57:32 +0300

    business dashboard stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,42 @@
-//import "bootstrap";
+import "bootstrap";
+import "jquery";
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navtabs = document.querySelector(".nav-item")
+    if (navtabs) {
+      // Javascript to enable link to tab
+      var url = document.location.toString();
+      if (url.match('#')) {
+        $('.nav-item a[href="#' + url.split('#')[1] + '"]').tab('show');
+      }
+
+      // Change hash for page-reload
+      $('.nav-item a').on('shown.bs.tab', function (e) {
+          window.location.hash = e.target.hash;
+      })
+    }
+  });
